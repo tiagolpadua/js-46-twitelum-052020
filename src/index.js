7ecd273b54cb272587d65c255ba84cb9bdbedd1c
F9342853 Tiago Lage Payne de Padua <tiagolpadua@bb.com.br>
2020-05-17 22:22:22 -0300

    c12

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-// CSSs Globais
-import "./assets/css/reset.css";
-import "./assets/css/container.css";
+import { BrowserRouter } from "react-router-dom";
 import "./assets/css/btn.css";
+import "./assets/css/container.css";
 import "./assets/css/icon.css";
 import "./assets/css/iconHeart.css";
 import "./assets/css/notificacao.css";
-
 import "./assets/css/novoTweet.css";
-// import './index.css';
-
-import App from "./App";
+// CSSs Globais
+import "./assets/css/reset.css";
+import Roteamento from "./routes";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <Roteamento />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,12 +1,12 @@
 import React, { Component, Fragment } from "react";
-import Cabecalho from "./components/Cabecalho";
-import NavMenu from "./components/NavMenu";
-import Dashboard from "./components/Dashboard";
-import Widget from "./components/Widget";
-import TrendsArea from "./components/TrendsArea";
-import Tweet from "./components/Tweet";
+import Cabecalho from "../../components/Cabecalho";
+import NavMenu from "../../components/NavMenu";
+import Dashboard from "../../components/Dashboard";
+import Widget from "../../components/Widget";
+import TrendsArea from "../../components/TrendsArea";
+import Tweet from "../../components/Tweet";
 
-class App extends Component {
+class Home extends Component {
   constructor() {
     super();
     this.state = {
@@ -87,4 +87,4 @@ ${this.state.novoTweet.length > 140 ? "novoTweet__status--invalido" : ""}
   }
 }
 
-export default App;
+export default Home;
