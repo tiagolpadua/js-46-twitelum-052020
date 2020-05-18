35291c6fd676ed4a4fd4191d1fbad6c8622801a2
F9342853 Tiago Lage Payne de Padua <tiagolpadua@bb.com.br>
2020-05-17 23:08:01 -0300

    c14

--- src/pages/LoginPage/index.js
+++ src/pages/LoginPage/index.js
@@ -8,7 +8,48 @@ import "./loginPage.css";
 class LoginPage extends Component {
   static contextType = NotificacaoContext;
 
-  // this.context.setMsg("Bem vindo ao Twitelum, login foi um sucesso!");
+  constructor(props) {
+    super(props);
+    this.state = {
+      msgErro: "",
+    };
+  }
+
+  fazerLogin = (infosDoEvento) => {
+    infosDoEvento.preventDefault();
+    const dadosDeLogin = {
+      login: this.refs.inputLogin.value,
+      senha: this.refs.inputSenha.value,
+    };
+    fetch("https://twitelum-api.herokuapp.com/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(dadosDeLogin),
+    })
+      .then(async (responseDoServer) => {
+        if (!responseDoServer.ok) {
+          const respostaDeErroDoServidor = await responseDoServer.json();
+          const errorObj = Error(respostaDeErroDoServidor.message);
+          errorObj.status = responseDoServer.status;
+          throw errorObj;
+        }
+        return responseDoServer.json();
+      })
+      .then((dadosDoServidorEmObj) => {
+        const token = dadosDoServidorEmObj.token;
+        if (token) {
+          localStorage.setItem("TOKEN", token);
+          this.context.setMsg("Bem vindo ao Twitelum, login foi um sucesso!");
+          this.props.history.push("/");
+        }
+      })
+      .catch((err) => {
+        console.error(`[Erro ${err.status}]`, err.message);
+        this.setState({ msgErro: err.message });
+      });
+  };
 
   render() {
     return (
@@ -18,12 +59,17 @@ class LoginPage extends Component {
           <div className="container">
             <Widget>
               <h2 className="loginPage__title">Seja bem vindo!</h2>
-              <form className="loginPage__form" action="/">
+              <form
+                className="loginPage__form"
+                action="/"
+                onSubmit={this.fazerLogin}
+              >
                 <div className="loginPage__inputWrap">
                   <label className="loginPage__label" htmlFor="login">
                     Login
                   </label>
                   <input
+                    ref="inputLogin"
                     className="loginPage__input"
                     type="text"
                     id="login"
@@ -35,15 +81,18 @@ class LoginPage extends Component {
                     Senha
                   </label>
                   <input
+                    ref="inputSenha"
                     className="loginPage__input"
                     type="password"
                     id="senha"
                     name="senha"
                   />
                 </div>
-                {/* <div className="loginPage__errorBox">
-                                    Mensagem de erro!
-                                </div> */}
+                {this.state.msgErro ? (
+                  <div className="loginPage__errorBox">
+                    {this.state.msgErro}
+                  </div>
+                ) : null}
                 <div className="loginPage__inputWrap">
                   <button className="loginPage__btnLogin" type="submit">
                     Logar
